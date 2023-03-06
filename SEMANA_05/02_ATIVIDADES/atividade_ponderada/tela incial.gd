6d795b4053a21ecc7a69dfa5855dc714a5117b98
LuizaRubim <luiza.rubim@sou.inteli.edu.br>
2023-03-05 22:23:39 -0300

    entrega ponderada semana 5, 2° tentativa

--- SEMANA_05/02_ATIVIDADES/atividade_ponderada/tela incial.gd
+++ SEMANA_05/02_ATIVIDADES/atividade_ponderada/tela incial.gd
@@ -9,7 +9,10 @@ var numero = 0
 var lista = []
 # variável nome adicionada de forma a ser declarada antes da função em si
 var nome
-
+# adicionado var para declarar a variável e retirando-a do looping para não resetar sua contagem
+var cont=0
+# adicionando var para declarar a variável
+var i =0
 
 func _on_Button_pressed():
 	#Coletando dados inseridos pelo usuário
@@ -17,6 +20,7 @@ func _on_Button_pressed():
 	# adicionando $ para identificar a LineEdit
 	numero = int($LineEdit.text)
 	#invertendo a ordem da variável para atribuir um valor a variável, e não uma variável ao valor
+	# adicionando a função split para separar o número do nome e adquirir apenas o nome
 	nome = $LineEdit.text.split(" ")[0]
 
 
@@ -26,25 +30,23 @@ func _on_Button2_pressed():
 	# alterando o nome da variável para o nome da declaração inicial
 		numero+=i
 		lista.append(numero)
-		
-	# adicionando str ao numero para trannsfomá-lo em string, ou seja, o tipo de dado adequado para atribuir a um Label
+	# adicionando str ao numero para transfomá-lo em string, ou seja, o tipo de dado adequado para atribuir a um Label
 	$Label.text = str(numero)
 
 
 func _on_Button3_pressed():
 	#Mudando o nome do usuário de acordo com os dados da lista
 	#Se houver algum número ímpar o nome deve adicionar "baldo" ao final
-	while(len(lista)):
-	# adicionado var para declarar a variável 
-		var cont=0
-	# adicionando var para declarar a variável
-		var i=0
+	# adicionando comparativo no while para manter um condição modificável após a leitura de todos os termos da lista e não rodar o looping infinitamente
+	while(len(lista)> i):
+	# modificando o local das variáveis para não resetá-las a cada looping
 		if(lista[i]%2==1):
 			cont+=1
+		i+=1
 		if(cont!=0):
 			nome = nome+"baldo"
-	#adicionando função break para realizar a operação de alterar o nome apenar uma vez
+	#adicionando função break para realizar a operação de alterar o nome apenas uma vez
 			break
+			
 	# retirando identação para trazer a atribuição de nome fora do looping
 	$Label2.text = nome
-	
